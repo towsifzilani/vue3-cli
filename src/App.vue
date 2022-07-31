2c79b6a2033922bd7f4f96fc0a732b63d07a4134
towsifzilani <towsifzilani@gmail.com>
2022-07-31 22:20:42 +0600

    New vuecli project walkthrough

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ export default {
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +24,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
